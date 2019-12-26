aa06974960e308f8f241955c7ea579f253ad61e7
anudeep0007 <anudeep950@gmail.com>
2019-12-26 15:11:09 +0530

    First COmmit

---
+++ E-Learning/Assets/Elephant/Model/SAI.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+
+public class SAI : MonoBehaviour
+{
+    private Animation eleanim;
+    public AnimationClip[] eleanimations;
+   
+    // Start is called before the first frame update
+
+
+    int animnum = 0;
+    void Awake()
+    {
+        eleanim = gameObject.GetComponent<Animation>();
+       
+
+
+    }
+
+
+    void Start()
+    {
+        StartCoroutine(wait());
+
+        if (eleanim)
+        {
+            eleanim.clip = eleanimations[animnum];
+            eleanim.Play();
+           
+            
+
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+       
+
+
+
+    }
+
+
+
+   IEnumerator wait()
+    {
+
+        yield return new WaitForSeconds(5);
+        animnum++;
+        clipchange();
+
+
+    }
+
+
+    private void clipchange()
+   {
+       
+            eleanim.Stop();
+            eleanim.clip = eleanimations[animnum];
+            eleanim.Play();
+
+        StartCoroutine(wait());
+      
+    }
+
+
+
+
+
+
+
+}
